a0d833cf1a9f252c1a4df3f820b719dc9e9f9656
Mauricio Oliveira <mauricio.oliveira8583@gmail.com>
2023-05-16 15:03:15 -0300

    Commit

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from core.models import Evento
+
+# Create your views here.
+
+def lista_eventos(request):
+    evento = Evento.objects.all()
+    dados = {'evento': evento}
+    return render(request, 'agenda.html', dados)
